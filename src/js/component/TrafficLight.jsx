3cdd218a940b85e968cd09957a4e4353be47ac06
JesseMiami100 <jessemetayer@gmail.com>
2022-10-02 00:42:36 +0000

    trafficLight

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+//create your first component
+const Home = () => {
+    const [lightColor, setLightColor] = useState("");
+    return (
+        <div id="container">
+            <div className="trafficTop"></div>
+            <div className="trafficBox">
+                <div
+                    className={
+                        lightColor === "red" ? "red redOff" : "redOff"}
+                    onClick={() => {
+                        setLightColor("red");
+                    }}></div>
+                <div
+                    className={
+                        lightColor === "yellow"
+                            ? "yellow yellowOff"
+                            : "yellowOff"
+                    }
+                    onClick={() => {
+                        setLightColor("yellow");
+                    }}></div>
+                <div
+                    className={
+                        lightColor === "green" ? "green GreenOff" : "greenOff"
+                    }
+                    onClick={() => {
+                        setLightColor("green");
+                    }}></div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
